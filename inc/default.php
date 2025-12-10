d964cb2d2a7e8065fa451e439cff1e5c269e1a9a
Md Shakibur Rahman <mdshakiburrahman6@gmail.com>
2025-12-10 15:26:38 +0600

    Update Theme support function

--- inc/default.php
+++ inc/default.php
@@ -4,8 +4,13 @@
 // Theme Deafult File
 // ===========================
 
-// Add Title 
-add_theme_support('title-tag');
+function cleancraft_theme_setup() {
 
-//  Add Blog 
-add_theme_support( 'post-thumbnails', array('page','post', 'portfolio','service') );
+    // Add Title Support
+    add_theme_support('title-tag');
+
+    // Add Thumbnail Support for multiple post types
+    add_theme_support('post-thumbnails', array('page', 'post', 'portfolio', 'service'));
+
+}
+add_action('after_setup_theme', 'cleancraft_theme_setup');
